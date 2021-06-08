ecbeb24608556cad8e55ca254017fb4b13c02009
lucimaster100 <74505266+lucimaster100@users.noreply.github.com>
2021-06-08 14:13:51 +0300

    Update DatabaseController.js

--- Controller/DatabaseController.js
+++ Controller/DatabaseController.js
@@ -1,26 +1,15 @@
-var mysql = require('mysql');
+var pgtools = require("pgtools");
+const config = {
+    user: "postgres",
+    host: "localhost",
+    password: "postgres",
+    port: 5432
+};
 
-var con = mysql.createConnection({
-    host: "127.0.0.1",
-    port: "8889",
-    user: "root",
-    password: "",
-    database: "mydb"
-
+pgtools.createdb(config, "some_db", function(err, res) {
+    if (err) {
+        console.error(err);
+        process.exit(-1);
+    }
+    console.log(res);
 });
-// con.connect(function(err) {
-//     console.log("Connected!");
-//     con.query("CREATE DATABASE Superhero", function (err, result) {
-//         console.log("Database created");
-//     });
-// });
-
-con.connect(function(err) {
-    console.log("Connected!");
-    var sql = "CREATE TABLE DcTable (page_id VARCHAR(255), name VARCHAR(255),urlslug VARCHAR(255),ID VARCHAR(255),ALIGN VARCHAR(255),EYE VARCHAR(255),HAIR VARCHAR(255),SEX VARCHAR(255),GSM VARCHAR(255),ALIVE VARCHAR(255),";
-    con.query(sql, function (err, result) {
-        console.log("Table created");
-    });
-});
-
-
