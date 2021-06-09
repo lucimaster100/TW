06bcca9a7a198ddc68f2fe93be2b51ba0efbc198
RobertCroitor <79307887+RobertCroitor@users.noreply.github.com>
2021-06-10 00:05:45 +0300

    Found a Solution

--- Views/JS/getContent/getHardContent.js
+++ Views/JS/getContent/getHardContent.js
@@ -6,13 +6,10 @@ const pool = new Pool({
     database: "Hero",
     port: 5432
 })
-pool.on('error', (err, client) => {
-    console.error('Error:', err);
-});
 
-let str = "";
 
-const query = `SELECT * FROM dc ORDER BY Random() LIMIT 1`;
+var array = []
+const query = `SELECT * FROM dc ORDER BY Random() LIMIT 2`;
 pool.connect((err, client, done) => {
     if (err) throw err;
     client.query(query, (err, res) => {
@@ -22,11 +19,16 @@ pool.connect((err, client, done) => {
         } else {
             const data = res.rows;
             data.forEach(row => {
-                console.log(`Name: ${row.name}`);
-                str += `${row.name}`
+                array.push(`${row.name}`)
             })
+            ouut()
         }
 
     });
 });
 
+
+function ouut() {
+    console.log(array[1])
+
+}
