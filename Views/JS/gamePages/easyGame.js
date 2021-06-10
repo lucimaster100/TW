0e333f12669afc1c650401ed3aac6f0e80df609b
RobertCroitor <79307887+RobertCroitor@users.noreply.github.com>
2021-06-10 12:36:48 +0300

    Nothing 4

--- Views/JS/gamePages/easyGame.js
+++ Views/JS/gamePages/easyGame.js
@@ -1,3 +1,4 @@
+
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
