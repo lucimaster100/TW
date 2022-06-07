f524fd21917444bec6f7dd4b0a746b6982ec21d2
lucimaster100 <74505266+lucimaster100@users.noreply.github.com>
2022-06-07 16:08:36 +0300

    asdf

--- js/server.js
+++ js/server.js
@@ -1,5 +1,8 @@
 const http = require('http')
 const { getProducts , getProduct,createProduct,updateProduct,deleteProduct }=require('../controllers/productController')
+const url = require('url')
+const fs =require('fs')
+const lookup = require('mime-types').lookup
 
 const server= http.createServer((req,res)=>{
     if(req.url=='/products'&& req.method=='GET'){
@@ -16,8 +19,27 @@ const server= http.createServer((req,res)=>{
         const id=req.url.split('/')[2]
         deleteProduct(req,res,id)
     }else{
-        res.writeHead(200,{'Content-Type':'application/json'})
-        res.end(JSON.stringify({ message : 'RouteNotFound'}))
+        let parsedURL=url.parse(req.url,true)
+        let path=parsedURL.path.replace(/^\/+|\/+$/g,"")
+        if(path== ""){
+            path="Home.html"
+        }
+        let file= "./Views/" +path
+        res.writeHead(200, {'Content-Type':'text/html'})
+        fs.readFile(file,function (error,content){
+            if(error){
+                res.writeHead(404)
+                res.write('File not found')
+                res.end()
+            }else {
+                let mime=lookup(path)
+                res.writeHead(200,{'Content-Type': mime})
+                res.write(content)
+                res.end()
+            }
+
+        })
+
     }
 
 })
