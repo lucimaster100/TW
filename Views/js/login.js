0f5a7b74e57df04762a5ce8b9e5b23de27a56920
RobertCroitor <79307887+RobertCroitor@users.noreply.github.com>
2022-06-11 16:36:03 +0300

    Changes

--- Views/js/login.js
+++ Views/js/login.js
@@ -1,28 +1,54 @@
+let username="ana";
+
+const fetchUserCredentials=async (username) =>{
+    let UserData= await fetch(`/usersByName/${username}`)
+    let UserDataJson= await UserData.json()
+    return UserDataJson
+}
+const getUserData=async()=>{
+    let jsonData=await fetchUserCredentials('username1')
+    console.log(jsonData[0])
+    console.log(jsonData[0].username)
+    console.log(jsonData[0].password)
+    
+}
+
 class Login {
     constructor(form, fields) {
         this.form = form;
         this.fields = fields;
         this.validateSubmit();
     }
+    
     validateSubmit() {
+        
         let self = this;
         this.form.addEventListener('submit', (e) => {
             e.preventDefault();
             var error = 0;
+            var count=0;
             self.fields.forEach((field) => {
                 const input = document.querySelector(`#${field}`);
+                if (count==0){
+         
+                    username=input.value;
+                }
+                count++;
                 if (self.validateFields(input) == false) {
                     error++;
                 }
                
             })
             if (error == 0) {
+              
                 localStorage.setItem("auth",1)
                 this.form.submit();
             }
         })
     }
     validateFields(field) {
+        console.log(username);
+        getUserData()
         if (field.value.trim() == "") {
             this.setStatus(
                 field,
