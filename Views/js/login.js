f9bd1613d0b2f4ed7edf28a857f86ab48dcb0d08
RobertCroitor <79307887+RobertCroitor@users.noreply.github.com>
2022-06-11 19:53:34 +0300

    exploit

--- Views/js/login.js
+++ Views/js/login.js
@@ -1,28 +1,49 @@
-let username="ana";
-let validUsername=0;
-const fetchUserCredentials=async (username) =>{
-    
+let username = "";
+localStorage.removeItem("user")
+localStorage.removeItem("id")
+localStorage.removeItem("auth")
+let i = 100
+
+const fetchUserCredentials = async (username) => {
+
+
     try {
+        //Daca e gasit , vreau sa fac validUsername = 1 , daca nu , sa ramana 0
+
         let UserData = await fetch(`/usersByName/${username}`)
         let UserDataJson = await UserData.json()
+        if (UserDataJson == 0) {
+          
+            return 0
+        }
+        else {
+            return UserDataJson
+        }
 
-        return UserDataJson
-    }catch (err){
+    } catch (err) {
         console.log(err)
     }
 }
-const getUserData=async(username)=>{
+const getUserData = async (username) => {
 
     try {
+
         let jsonData = await fetchUserCredentials(username)
-        console.log(jsonData[0].username)
-        console.log(jsonData[0].password)
-        
-    }catch (err){
-        
+        let user = jsonData[0].username
+        let pass = jsonData[0].password
+        let id = jsonData[0].id
+
+ 
+        localStorage.setItem("user", user)
+        localStorage.setItem("id", id)
+    } catch (err) {
+
+
     }
-   
+
 }
+
+
 
 class Login {
     constructor(form, fields) {
@@ -30,40 +51,56 @@ class Login {
         this.fields = fields;
         this.validateSubmit();
     }
-    
+
     validateSubmit() {
         
         let self = this;
         this.form.addEventListener('submit', (e) => {
+           
             e.preventDefault();
             var error = 0;
-            var count=0;
-  
-            console.log(validUsername)
+            var count = 0;
+
+
             self.fields.forEach((field) => {
                 const input = document.querySelector(`#${field}`);
-                if (count==0){
-                   
-                    username=input.value;
-                    getUserData(username);
+
+                if (count == 0) {
+                    username = input.value;
+                    
+                    getUserData(username)
+                }
+                if (count == 1) {
+                    username = input.value;
+                    
+                    getUserData(username)
                 }
                 count++;
                 if (self.validateFields(input) == false) {
                     error++;
                 }
-               
+
             })
+          
             if (error == 0) {
-              
-                localStorage.setItem("auth",1)
+                localStorage.setItem("auth", 1)
                 this.form.submit();
+            }else{
+                
             }
         })
     }
+
+
+
+
     validateFields(field) {
-        console.log(validUsername);
-        getUserData(username);
-        console.log(validUsername);
+
+
+        
+
+
+
         if (field.value.trim() == "") {
             this.setStatus(
                 field,
@@ -72,9 +109,10 @@ class Login {
             );
 
             return false;
-        } else {
+        }
+        else {
             if (field.type == "password") {
-                
+
                 if (field.value.length < 8) {
                     this.setStatus(
                         field,
@@ -82,12 +120,12 @@ class Login {
                         "error"
                     );
                     return false;
-                }else{
-                    this.setStatus(field,null,"success");
+                } else {
+                    this.setStatus(field, null, "success");
                     return true;
                 }
-            }else{
-                this.setStatus(field,null,"success");
+            } else {
+                this.setStatus(field, null, "success");
                 return true;
             }
         }
