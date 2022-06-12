a586f364df4158cdfdfa176e9da6690f7da2305b
RobertCroitor <79307887+RobertCroitor@users.noreply.github.com>
2022-06-12 19:58:28 +0300

    Changeeee

--- Views/js/fetchUserData.js
+++ Views/js/fetchUserData.js
@@ -1,17 +1,19 @@
-function fetchUserProfile(){
-    var user_id=localStorage.getItem('id')
-    fetch(`/userProfileById/${user_id}`).then(response =>{
-        if(!response.ok)
-            throw Error(ERROR)
-        return response.json()
-    }).then(data =>{
-        var image
-        if(!data.image){
-            image='photos/defaultImages/DefaultUser.jpg'
-        }else{
-            image=user.image
-        }
-        const html=`<div class="avatar">
+function fetchUserProfile() {
+  var user_id = localStorage.getItem("id");
+  fetch(`/userProfileById/${user_id}`)
+    .then((response) => {
+      if (!response.ok) throw Error(ERROR);
+      return response.json();
+    })
+    .then((data) => {
+      var image;
+
+      if (!data.img) {
+        image = "photos/defaultImages/DefaultUser.jpg";
+      } else {
+        image = data.img;
+      }
+      const html = `<div class="avatar">
             <img src="${image}" height="150" width="150" alt="Error">
 
         </div>
@@ -19,11 +21,12 @@ function fetchUserProfile(){
         <h2>My Profile </h2>
         <div class="paragraphs">
             <p>${data.description}</p>
-        </div>`
-        document.querySelector(".profileSection").innerHTML = html
-    }).catch(error => {
-        console.log(error)
+        </div>`;
+      document.querySelector(".profileSection").innerHTML = html;
     })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-fetchUserProfile()
+fetchUserProfile();
