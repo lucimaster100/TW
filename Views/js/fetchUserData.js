f7dbb93d4843a970db1507e6c9872bb90d7a65a7
RobertCroitor <79307887+RobertCroitor@users.noreply.github.com>
2022-06-12 20:02:53 +0300

    Revert "Changeeee"
    This reverts commit a586f364df4158cdfdfa176e9da6690f7da2305b.

--- Views/js/fetchUserData.js
+++ Views/js/fetchUserData.js
@@ -1,19 +1,17 @@
-function fetchUserProfile() {
-  var user_id = localStorage.getItem("id");
-  fetch(`/userProfileById/${user_id}`)
-    .then((response) => {
-      if (!response.ok) throw Error(ERROR);
-      return response.json();
-    })
-    .then((data) => {
-      var image;
-
-      if (!data.img) {
-        image = "photos/defaultImages/DefaultUser.jpg";
-      } else {
-        image = data.img;
-      }
-      const html = `<div class="avatar">
+function fetchUserProfile(){
+    var user_id=localStorage.getItem('id')
+    fetch(`/userProfileById/${user_id}`).then(response =>{
+        if(!response.ok)
+            throw Error(ERROR)
+        return response.json()
+    }).then(data =>{
+        var image
+        if(!data.image){
+            image='photos/defaultImages/DefaultUser.jpg'
+        }else{
+            image=user.image
+        }
+        const html=`<div class="avatar">
             <img src="${image}" height="150" width="150" alt="Error">
 
         </div>
@@ -21,12 +19,11 @@ function fetchUserProfile() {
         <h2>My Profile </h2>
         <div class="paragraphs">
             <p>${data.description}</p>
-        </div>`;
-      document.querySelector(".profileSection").innerHTML = html;
+        </div>`
+        document.querySelector(".profileSection").innerHTML = html
+    }).catch(error => {
+        console.log(error)
     })
-    .catch((error) => {
-      console.log(error);
-    });
 }
 
-fetchUserProfile();
+fetchUserProfile()
--- Views/js/products.js
+++ Views/js/products.js
@@ -1,49 +1,57 @@
 function fetchProducts() {
-  fetch("/products")
-    .then((response) => {
-      if (!response.ok) throw Error(ERROR);
-      return response.json();
-    })
-    .then((data) => {
-      const html = data
-        .map((product) => {
-          var image;
-          if (!product.image) {
-            image = "photos/defaultImages/defaultProduct.png";
-          } else {
-            image = product.image;
-          }
+    fetch('/products').then(response => {
+        if (!response.ok)
+            throw Error(ERROR)
+        return response.json()
+    }).then(data => {
+        const html = data.map(product => {
+            var image
+            if (!product.image) {
+                image = 'photos/defaultImages/defaultProduct.png'
+            } else {
+                image = product.image
+            }
 
-          var type;
-          if (!product.type) type = "Not Found";
-          else type = product.type;
+            var type
+            if (!product.type)
+                type = "Not Found"
+            else
+                type = product.type
 
-          var price;
-          if (!product.price) price = "Not Found";
-          else price = product.price;
+            var price
+            if (!product.price)
+                price = "Not Found"
+            else
+                price = product.price
 
-          var title;
-          if (!product.title) title = "Not Found";
-          else title = product.title;
+            var title
+            if (!product.title)
+                title = "Not Found"
+            else
+                title = product.title
 
-          var origin;
-          if (!product.origin) origin = "Not Found";
-          else origin = product.origin;
+            var origin
+            if (!product.origin)
+                origin = "Not Found"
+            else
+                origin = product.origin
 
-          var utilisation;
-          if (!product.utilisation) utilisation = "Not Found";
-          else utilisation = product.utilisation;
+            var utilisation
+            if (!product.utilisation)
+                utilisation = "Not Found"
+            else
+                utilisation = product.utilisation
 
-          var label;
-          if (!product.label) label = "Without Label";
-          else {
-            if (product.label == false) label = "Without Label";
-            else label = "With Label";
-          }
-          var views;
-          if (!product.views) views = "0";
-          else views = product.views;
-          return `
+            var label
+            if (!product.label)
+                label = "Not Found"
+            else {
+                if (product.label==false)
+                    label = "Without Label"
+                else
+                    label="With Label"
+                }
+            return `
             
             <div class="column">
             <div class="card">
@@ -61,20 +69,17 @@ function fetchProducts() {
                             <p class="cardText">Origin : ${origin}</p>
                             <p class="cardText">State : ${utilisation}</p>
                             <p class="cardText">${label}</p>
-                            <p class="cardText">Likes : ${views}</p>
-                            <a  href="productPage.html" onclick="localStorage.setItem('product_id',${product.id})">Like</a>
+                            <a  href="productPage.html" onclick="localStorage.setItem('product_id',${product.id})">Visit</a>
                         </div>
                         </div>
                     </div>
                 </div>
-            </div>`;
-        })
-        .join("");
-      document.querySelector(".row").innerHTML = html;
+            </div>`
+        }).join('')
+        document.querySelector(".row").innerHTML = html
+    }).catch(error => {
+        console.log(error)
     })
-    .catch((error) => {
-      console.log(error);
-    });
 }
 
 fetchProducts();
