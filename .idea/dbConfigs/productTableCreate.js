2f380f82d4d7a7d924ab06fee508a133e9de31e4
RobertCroitor <79307887+RobertCroitor@users.noreply.github.com>
2022-06-12 18:18:23 +0300

    Nothing

--- .idea/dbConfigs/productTableCreate.js
+++ .idea/dbConfigs/productTableCreate.js
@@ -1,27 +1,24 @@
-
-const { Client, Pool } = require('pg')
+const { Client, Pool } = require("pg");
 
 const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: 'postgres',
-    database: 'postgres'
-})
-
-
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "postgres",
+  database: "postgres",
+});
 
 const execute = async (query) => {
-    try {
-        await client.connect();     // gets connection
-        await client.query(query);  // sends queries
-        return true;
-    } catch (error) {
-        console.error(error.stack);
-        return false;
-    } finally {
-        await client.end();         // closes connection
-    }
+  try {
+    await client.connect(); // gets connection
+    await client.query(query); // sends queries
+    return true;
+  } catch (error) {
+    console.error(error.stack);
+    return false;
+  } finally {
+    await client.end(); // closes connection
+  }
 };
 
 const text = `
@@ -34,11 +31,12 @@ const text = `
     "utilisation" text NOT NULL,
     "label" boolean NOT NULL,
     "image" text NOT NULL,
-    "user_id" integer NOT NULL
+    "user_id" integer NOT NULL,
+    "views" integer DEFAULT 0
     );`;
 
-execute(text).then(result => {
-    if (result) {
-        console.log('Table created');
-    }
+execute(text).then((result) => {
+  if (result) {
+    console.log("Table created");
+  }
 });
